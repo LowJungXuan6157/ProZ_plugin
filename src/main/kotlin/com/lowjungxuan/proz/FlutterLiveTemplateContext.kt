65633e66ac41a7e31a7068b176a0049bb86d227a
LowJungXuan6157 <124644086+LowJungXuan6157@users.noreply.github.com>
2023-05-14 21:38:11 +0800

    Bloc live template Completed

--- src/main/kotlin/com/lowjungxuan/proz/FlutterLiveTemplateContext.kt
+++ src/main/kotlin/com/lowjungxuan/proz/FlutterLiveTemplateContext.kt
@@ -3,7 +3,7 @@ package com.lowjungxuan.proz
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class GetXContext : TemplateContextType("FLUTTER", "Flutter") {
+class FlutterLiveTemplateContext : TemplateContextType("FLUTTER", "Flutter") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file.name.endsWith(".dart")
     }
